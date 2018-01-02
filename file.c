2fc51e555bbe1f8c0fe0e53abe83753efa9a8c4b
nipunbharti <nipunbharti21@gmail.com>
2018-01-02 20:26:26 +0530

    Entering data for first semester implemented

--- file.c
+++ file.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct student{
 	char name[10];
 	char rollno[10];
 	char gender;
-	float g1,g2,g3,g4,gpa,cgpa;
-};
+	int g1,g2,g3,g4;
+	float gpa,cgpa;
+}s;
 
 void insert();
+void enterdata();
 
 int main()
 {
@@ -32,5 +35,40 @@ int main()
 
 void insert()
 {
+	int sem = 0;
+	float x;
+	if(sem==0){
+		FILE *f;
+		int n=0;
+		f = fopen("1.txt","w+");
+		while(n<5){
+			printf("Enter data for %d student:\n",n+1);
+			printf("Enter name:\n");
+			scanf("%s",s.name);
+			printf("Enter the roll no:\n");
+			scanf("%s",s.rollno);
+			printf("Enter the gender: M or F:\n");
+			scanf(" %c",&s.gender);
+			enterdata();
+			s.gpa = (s.g1 + s.g2 + s.g3 +s.g4)/4;
+			s.cgpa = (s.g1 + s.g2 + s.g3 +s.g4)/4;
+			x = s.cgpa;
+			fprintf(f,"%s %s %c %d %d %d %d %0.2f %0.2f\n",s.name,s.rollno,s.gender,s.g1,s.g2,s.g3,s.g4,s.gpa,s.cgpa);
+			n++;
+		}
+		fclose(f);
+		sem++;
+	}
+}
 
+void enterdata()
+{
+	printf("Enter the grades for COM201T\n");
+	scanf("%d",&s.g1);
+	printf("Enter the grades for MAN201T\n");
+	scanf("%d",&s.g2);
+	printf("Enter the grades for ELE201T\n");
+	scanf("%d",&s.g3);
+	printf("Enter the grades for MAT201T\n");
+	scanf("%d",&s.g4);
 }
