ca436c3f6b527c995feafd3028055f08a9f731cf
nipunbharti <nipunbharti21@gmail.com>
2018-01-02 22:03:28 +0530

    Scanning the file for CGPA>7.5 done for 1 record, half for second

--- file.c
+++ file.c
@@ -13,18 +13,21 @@ struct detail{
 
 void insert();
 void enterdata();
+void greater();
 
 int main()
 {
 	int ch=0;
 	while(1){
-		printf("1.Enter and insert record\n2.Exit\n");
+		printf("1.Enter and insert record\n2.Name of students whose CGPA is greater than 7.5\n3.Exit\n");
 		scanf("%d",&ch);
 		switch(ch)
 		{
 			case 1:
 				insert();
 				break;
+			case 2:
+				greater();
 			case 4:
 				exit(0);
 				break;
@@ -68,13 +71,14 @@ void insert()
 			printf("Enter data for %d student:\n",n+1);
 			enterdata();
 			s.gpa = (s.g1 + s.g2 + s.g3 +s.g4)/4.0;
-			s.cgpa = (s.cgpa + s.gpa)/2;
+			s.cgpa = (s.cgpa + s.gpa)/2.0;
 			fprintf(f,"%s %s %c %d %d %d %d %0.2f %0.2f\n",d[n].name,d[n].rollno,d[n].gender,s.g1,s.g2,s.g3,s.g4,s.gpa,s.cgpa);
 			n++;
 		}
 		fclose(f);
 		sem++;
 	}
+	n=0;
 	if(sem==2){
 		FILE *f;
 		f = fopen("3.txt","w+");
@@ -83,22 +87,23 @@ void insert()
 			printf("Enter data for %d student:\n",n+1);
 			enterdata();
 			s.gpa = (s.g1 + s.g2 + s.g3 +s.g4)/4.0;
-			s.cgpa = (s.cgpa + s.gpa)/2;
+			s.cgpa = (s.cgpa + s.gpa)/2.0;
 			fprintf(f,"%s %s %c %d %d %d %d %0.2f %0.2f\n",d[n].name,d[n].rollno,d[n].gender,s.g1,s.g2,s.g3,s.g4,s.gpa,s.cgpa);
 			n++;
 		}
 		fclose(f);
 		sem++;
 	}
+	n=0;
 	if(sem==3){
 		FILE *f;
-		f = fopen("3.txt","w+");
+		f = fopen("4.txt","w+");
 		while(n<2){
 			printf("For fourth sem:\n");
 			printf("Enter data for %d student:\n",n+1);
 			enterdata();
 			s.gpa = (s.g1 + s.g2 + s.g3 +s.g4)/4.0;
-			s.cgpa = (s.cgpa + s.gpa)/2;
+			s.cgpa = (s.cgpa + s.gpa)/2.0;
 			fprintf(f,"%s %s %c %d %d %d %d %0.2f %0.2f\n",d[n].name,d[n].rollno,d[n].gender,s.g1,s.g2,s.g3,s.g4,s.gpa,s.cgpa);
 			n++;
 		}
@@ -116,4 +121,45 @@ void enterdata()
 	scanf("%d",&s.g3);
 	printf("Enter the grades for MAT201T\n");
 	scanf("%d",&s.g4);
+}
+
+void greater()
+{
+	FILE *f;
+	float x;
+	f = fopen("4.txt","a+");
+	char c;
+	int move=1,space=0;
+	while(space!=8){
+		c = getc(f);
+		if(c == 32){
+			space++;
+		}
+		fseek(f,move,SEEK_SET);
+		move++;
+	}
+	fscanf(f,"%f",&x);
+	if(x>7.5){
+		fseek(f,0,SEEK_SET);
+		c = getc(f);
+		while(c!=32){
+			printf("%c",c);	
+			c = getc(f);
+		}
+	}
+	while(space!=16){
+		c = getc(f);
+		if(c == 32){
+			space++;
+		}
+		fseek(f,move,SEEK_SET);
+		move++;
+	}
+	fscanf(f,"%f",&x);
+	if(x>7.5){
+		fseek(f,0,SEEK_SET);
+		c = getc(f);
+		printf("%c",c);
+	}
+	fclose(f);
 }
