1a78694a92cbfff14bda3f87d04e4d6a3e290b1c
Nipun Bharti <nipunbharti21@gmail.com>
2018-01-08 08:47:52 +0530

    Sem and course 10 pointers added

--- file.c
+++ file.c
@@ -16,12 +16,13 @@ void insert();
 void enterdata();
 void greater();
 void check();
+void tenPoint();
 
 int main()
 {
 	int ch=0;
 	while(1){
-		printf("1.Enter and insert record\n2.Name of students whose CGPA is greater than 7.5\n3.Name of students whose name start with given starting characters\n4.Exit\n");
+		printf("1.Enter and insert record\n2.Name of students whose CGPA is greater than 7.5\n3.Name of students whose name start with given starting characters\n4.Sem and Course 10 pointer\n5.Exit\n");
 		scanf("%d",&ch);
 		switch(ch)
 		{
@@ -35,6 +36,9 @@ int main()
 				check();
 				break;
 			case 4:
+				tenPoint();
+				break;
+			case 5:
 				exit(0);
 				break;
 			default:
@@ -233,4 +237,648 @@ void check()
 		r=0;
 	}
 	fclose(f);
+}
+
+void tenPoint()
+{
+	int ch;
+	char course[7];
+	printf("Enter the semester:");
+	scanf("%d",&ch);
+	if(ch==1){
+		FILE *f;
+		f = fopen("1.txt","a+");
+		printf("Enter the course:");
+		scanf("%s",course);
+		if(strcmp(course,"COM201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 3)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		if(strcmp(course,"MAN201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 4)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		if(strcmp(course,"MAT201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 5)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		if(strcmp(course,"ELE201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 6)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		fclose(f);
+	}
+	if(ch==2){
+		FILE *f;
+		f = fopen("2.txt","a+");
+		printf("Enter the course:");
+		scanf("%s",course);
+		if(strcmp(course,"COM201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 3)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		if(strcmp(course,"MAN201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 4)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		if(strcmp(course,"MAT201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 5)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		if(strcmp(course,"ELE201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 6)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		fclose(f);
+	}
+	if(ch==3){
+		FILE *f;
+		f = fopen("3.txt","a+");
+		printf("Enter the course:");
+		scanf("%s",course);
+		if(strcmp(course,"COM201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 3)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		if(strcmp(course,"MAN201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 4)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		if(strcmp(course,"MAT201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 5)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		if(strcmp(course,"ELE201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 6)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		fclose(f);
+	}
+	if(ch==4){
+		FILE *f;
+		f = fopen("4.txt","a+");
+		printf("Enter the course:");
+		scanf("%s",course);
+		if(strcmp(course,"COM201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 3)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		if(strcmp(course,"MAN201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 4)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		if(strcmp(course,"MAT201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 5)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		if(strcmp(course,"ELE201T")==0){
+			char c;
+			int move=1,space=0,i=0,r=0,x;
+			for(i=0;i<5;i++){
+				while(space != (8*i + 6)){
+					c = getc(f);
+					if(c == 32){
+						space++;
+					}
+					fseek(f,move,SEEK_SET);
+					move++;
+				}
+				fscanf(f,"%d",&x);
+				if(x==10){
+					fseek(f,0,SEEK_SET);
+					c = getc(f);
+					if(i!=0){
+						move=1;
+						while(r<i){
+							while(c!='\n'){
+								fseek(f,move,SEEK_SET);
+								c = getc(f);
+								move++;
+							}
+							c = getc(f);
+							r++;
+						}	
+					}
+					while(c!=32){
+						printf("%c",c);	
+						c = getc(f);
+					}
+					printf("\n");
+				}
+				r=0,space=0,move=1;
+				fseek(f,move,SEEK_SET);
+			}
+		}
+		fclose(f);
+	}
 }
